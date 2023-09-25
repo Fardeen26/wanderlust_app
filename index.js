e28e79a1dbe590c1b5eb72c501fde45b5c7e90d7
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2023-09-25 19:17:28 +0530

    index route added with data

--- index.js
+++ index.js
@@ -15,19 +15,24 @@ app.get("/", (req, res) => {
     res.send("Hy, next big thing in the wolrd");
 });
 
-app.get("/testlisting", async (req, res) => {
-    let sampleListing = new Listing({
-        title: "Elon Musk House 8BHK",
-        description: "This benglow is the Elon Musk's Bunglow that is worls richest men made from new technologies",
-        price: 58966,
-        location: "Silicon Vally, Califonia",
-        country: "United States of America",
-    });
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({})
+    res.render("listings/index.ejs", { allListings });
+});
 
-    await sampleListing.save();
-    console.log("Sample was saved");
-    res.send("Jao DB ke Terminal me jake dekho");
-})
+// app.get("/testlisting", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "Elon Musk House 8BHK",
+//         description: "This benglow is the Elon Musk's Bunglow that is worls richest men made from new technologies",
+//         price: 58966,
+//         location: "Silicon Vally, Califonia",
+//         country: "United States of America",
+//     });
+
+//     await sampleListing.save();
+//     console.log("Sample was saved");
+//     res.send("Jao DB ke Terminal me jake dekho");
+// })
 app.listen(8080, () => {
     console.log("Listning to port 8080");
 })
