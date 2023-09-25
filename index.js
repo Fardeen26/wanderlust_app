5c1b2f81a781414a2a82d58da2fe5dc7ee8f2c05
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2023-09-25 21:03:22 +0530

    new files added

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const app = express();
 const mongoose = require("mongoose");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 main().then(() => {
     console.log("Connected to MongoDB");
 }).catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+    await mongoose.connect(MONGO_URL);
 }
 app.get("/", (req, res) => {
     res.send("Hy, next big thing in the wolrd");
@@ -19,6 +26,71 @@ app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({})
     res.render("listings/index.ejs", { allListings });
 });
+
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+});
+
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+app.get("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+});
+
+app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+});
+
+app.delete("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+});
+
+app.listen(8080, () => {
+    console.log("Listning to port 8080");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // app.get("/testlisting", async (req, res) => {
 //     let sampleListing = new Listing({
@@ -33,6 +105,3 @@ app.get("/listings", async (req, res) => {
 //     console.log("Sample was saved");
 //     res.send("Jao DB ke Terminal me jake dekho");
 // })
-app.listen(8080, () => {
-    console.log("Listning to port 8080");
-})
