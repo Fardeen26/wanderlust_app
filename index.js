cca53c42a1c0e59e3f6adcd72033c59a46a4dad2
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2023-09-25 18:31:01 +0530

    Add files

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const Listing = require("./models/listing.js");
+
+main().then(() => {
+    console.log("Connected to MongoDB");
+}).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+app.get("/", (req, res) => {
+    res.send("Hy, next big thing in the wolrd");
+});
+
+app.get("/testlisting", async (req, res) => {
+    let sampleListing = new Listing({
+        title: "Elon Musk House 8BHK",
+        description: "This benglow is the Elon Musk's Bunglow that is worls richest men made from new technologies",
+        price: 58966,
+        location: "Silicon Vally, Califonia",
+        country: "United States of America",
+    });
+
+    await sampleListing.save();
+    console.log("Sample was saved");
+    res.send("Jao DB ke Terminal me jake dekho");
+})
+app.listen(8080, () => {
+    console.log("Listning to port 8080");
+})
